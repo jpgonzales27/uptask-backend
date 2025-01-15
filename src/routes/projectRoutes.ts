719a995ade2a58e0c15d5f5edc7eb8801d4dfa9a
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-15 08:11:08 -0400

    instalando express validator y creeando middleware de validacion

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
 router.get("/", ProjectController.getAllProjects);
-router.post("/", ProjectController.createProject);
+router.post(
+  "/",
+  body("projectName").notEmpty().withMessage("El nombre del proyecto es obligatorio"),
+  body("clientName").notEmpty().withMessage("El nombre del cliente es obligatorio"),
+  body("description").notEmpty().withMessage("las descripcion del proyecto es obligatoria"),
+  handleInputErrors,
+  ProjectController.createProject
+);
 
 export default router;
