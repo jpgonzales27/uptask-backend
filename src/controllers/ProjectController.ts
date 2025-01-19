05861429a6964e5d60a69f490fadaba8b62d20b1
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-19 19:11:01 -0400

    cambiando el update de proyecto

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -40,12 +40,16 @@ export class ProjectController {
   static updateProject = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
     try {
-      const project = await Project.findByIdAndUpdate(id, req.body);
+      const project = await Project.findById(id);
       if (!project) {
         const error = new Error("Proyecto no encontrado");
         res.status(404).json({ error: error.message });
         return;
       }
+      project.clientName = req.body.clientName;
+      project.projectName = req.body.projectName;
+      project.description = req.body.description;
+
       await project.save();
       res.send("Proyecto actualizado correctamente");
       return;
