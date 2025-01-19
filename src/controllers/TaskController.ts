b17ea8d6f7129fa37cf8ad64799d497e370abb87
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-18 23:00:24 -0400

    actualizar tarea

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -48,4 +48,28 @@ export class TaskController {
       res.status(500).json({ error: "Hubo un error" });
     }
   };
+
+  static updateTask = async (req: Request, res: Response) => {
+    try {
+      const { taskId } = req.params;
+      const task = await Task.findByIdAndUpdate(taskId, req.body);
+      if (!task) {
+        const error = new Error("Tarea no encontrada");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      console.log(task.project);
+      console.log(req.project.id);
+
+      if (task.project.toString() !== req.project.id) {
+        const error = new Error("Accion no valida");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+      res.send("Tarea actualizada correctamente");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
+  };
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -58,4 +58,13 @@ router.get(
   TaskController.getProjectTaskById
 );
 
+router.put(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("ID no válido"),
+  body("name").notEmpty().withMessage("El Nombre de la tarea es Obligatorio"),
+  body("description").notEmpty().withMessage("La descripción de la tarea es obligatoria"),
+  handleInputErrors,
+  TaskController.updateTask
+);
+
 export default router;
