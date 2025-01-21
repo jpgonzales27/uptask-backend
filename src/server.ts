6835327b9efb3c75022e5084d958b3126bf5be9e
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-21 08:57:03 -0400

    configurando cors para habilitar conexiones dek frontend

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 
 dotenv.config();
 connectDB();
@@ -10,6 +12,7 @@ const app = express();
 
 // Leer datos de formularios(Body Request)
 app.use(express.json());
+app.use(cors(corsConfig));
 
 // Routes
 app.use("/api/projects", projectRoutes);
