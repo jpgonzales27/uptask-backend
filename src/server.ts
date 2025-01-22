ef1b8d932cd81e6cd02f3a019dbeec41e0a2afb4
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-22 10:40:11 -0400

    agregando morgan para ver los logs de las peticiones

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,19 @@ import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
 import cors from "cors";
 import { corsConfig } from "./config/cors";
+import morgan from "morgan";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors(corsConfig));
+
+// Logging
+app.use(morgan("dev"));
 
 // Leer datos de formularios(Body Request)
 app.use(express.json());
-app.use(cors(corsConfig));
 
 // Routes
 app.use("/api/projects", projectRoutes);
