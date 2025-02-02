f986363fd1fb014e6946f90055063a2f30698f26
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-02-01 22:33:24 -0400

    moviendo el codigo para encryptar password a un util

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,14 +1,13 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-import bcrypt from "bcrypt";
+import { hashPassword } from "../utils/auth";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
       const { password } = req.body;
       const user = new User(req.body);
       //hash password
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
+      user.password = await hashPassword(password);
       await user.save();
 
       res.send("Cuenta creada, revisa tu email para confirmarla");
