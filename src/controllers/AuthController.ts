c79425ac388834eb2fa62b41338da07e37bfb073
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-02-01 22:23:47 -0400

    creado controlador para crear cuenta

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,15 @@
 import type { Request, Response } from "express";
+import User from "../models/User";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
-    res.send("Create account api/auth");
+    try {
+      const user = new User(req.body);
+      await user.save();
+
+      res.send("Cuenta creada, revisa tu email para confirmarla");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
   };
 }
