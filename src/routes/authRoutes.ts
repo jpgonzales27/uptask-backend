51759cadff14168fb50416c87aa0707e515db5ed
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-02-01 22:18:43 -0400

    creado ruta create-account y validaciones

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import { body, param } from "express-validator";
+import { AuthController } from "../controllers/AuthController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World auth");
-});
+router.post(
+  "/create-account",
+  body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
+  body("password").isLength({ min: 8 }).withMessage("El password es muy corto, minimo 8 caracteres"),
+  body("password_confirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Los Password no son iguales");
+    }
+    return true;
+  }),
+  body("email").isEmail().withMessage("E-mail no válido"),
+  handleInputErrors,
+  AuthController.createAccount
+);
 
 export default router;
