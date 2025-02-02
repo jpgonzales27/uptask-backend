aacc97a68a51ed640626208193ac3889061e2f48
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-02-01 22:30:19 -0400

    encriptando el password con bcrypt

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,14 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-
+import bcrypt from "bcrypt";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { password } = req.body;
       const user = new User(req.body);
+      //hash password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
       await user.save();
 
       res.send("Cuenta creada, revisa tu email para confirmarla");
