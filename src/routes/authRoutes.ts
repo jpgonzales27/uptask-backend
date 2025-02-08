ee7312612081c417564b71d5a4c612fc2a43a73b
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-02-07 21:11:15 -0400

    Creando endpoint para resetear password

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -42,4 +42,11 @@ router.post(
   AuthController.requestConfirmationCode
 );
 
+router.post(
+  "/forgot-password",
+  body("email").isEmail().withMessage("E-mail no válido"),
+  handleInputErrors,
+  AuthController.forgotPassword
+);
+
 export default router;
